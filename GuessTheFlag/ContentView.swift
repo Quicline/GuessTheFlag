2948d74402303be15f8aa8cb4903d9dad418e2dd
Quicline <armando17171771@gmail.com>
2024-10-21 10:26:28 -0400

    Adding buttoms and first texts

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,19 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var showAlert = false
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+
     
     var body: some View {
         
-        Button("Show alert", role: .destructive) {
-            showAlert = true
-        }
-        .buttonStyle(.borderedProminent)
-        .alert("DANGER", isPresented: $showAlert) {
-            //Button("Cancel") { }
-            Button("Delete", role: .destructive) { }
-        } message: {
-            Text("gol")
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(countries[number])
+                    }
+                    
+                }
+            }
         }
         
         
